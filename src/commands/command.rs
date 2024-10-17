f2cfe15eb649ebe6693538b9c14cae8befd6bec9
feraxhp <feraxhp+gh@gmail.com>
2024-10-17 17:31:11 +0000

    + dimensions args added

--- src/commands/command.rs
+++ src/commands/command.rs
@@ -1,14 +1,29 @@
-use clap::{arg, command, crate_description, Command};
+use clap::{arg, command, crate_description, value_parser, Arg, ArgAction, Command};
+use color_print::cformat;
+use crate::formats::supported::SUPPORTED_INPUT_FORMATS;
 use crate::formats::validate::input;
 
 pub(crate) fn cim_command() -> Command {
     let parser = input::validate;
     command!()
         .about(crate_description!())
+        .disable_help_flag(true)
         .args([
-            arg!(<format> "The desire format")
+            arg!(<format> "Desired format for the converted image.")
                 .value_parser(parser)
             ,
+            dimension_arg("height", 'h'),
+            dimension_arg("width", 'w'),
+            arg!(--help "Prints this!").action(ArgAction::HelpLong)
         ])
+}
 
+fn dimension_arg(id: &'static str, short: char) -> Arg {
+    Arg::new(id.clone())
+        .short(short.clone())
+        .help(cformat!("The <g,b>{}</> dimension for <r,i>svg conversion</>", id.clone()))
+        .default_value("0")
+        .required(false)
+        .value_parser(value_parser!(u32))
+        .hide_default_value(true)
 }
